e67f7d633f23815ebc7e5bd8e90c6988768cd3db
Pavel Korzhenko <pksmall@live.ru>
2019-12-02 21:04:06 +0200

    fix folder structure

---
+++ lession03-t1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lession03_t1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
