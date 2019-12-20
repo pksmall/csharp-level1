7c019d9fde650b21a1b4e74b86c035d88604ca66
Pavel Korzhenko <pksmall@live.ru>
2019-12-20 12:25:46 +0200

    homework07 . task01 complete

---
+++ lesson07/task0701/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace task0701
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnCommand1 = new System.Windows.Forms.Button();
+            this.btnCommand2 = new System.Windows.Forms.Button();
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblCountCommands = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menu1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.commandToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abountToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnUndo = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnCommand1
+            // 
+            this.btnCommand1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.85714F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnCommand1.Location = new System.Drawing.Point(417, 34);
+            this.btnCommand1.Name = "btnCommand1";
+            this.btnCommand1.Size = new System.Drawing.Size(192, 62);
+            this.btnCommand1.TabIndex = 0;
+            this.btnCommand1.Text = "+1";
+            this.btnCommand1.UseVisualStyleBackColor = true;
+            this.btnCommand1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnCommand2
+            // 
+            this.btnCommand2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.85714F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnCommand2.Location = new System.Drawing.Point(420, 114);
+            this.btnCommand2.Name = "btnCommand2";
+            this.btnCommand2.Size = new System.Drawing.Size(191, 62);
+            this.btnCommand2.TabIndex = 1;
+            this.btnCommand2.Text = "x2";
+            this.btnCommand2.UseVisualStyleBackColor = true;
+            this.btnCommand2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.AutoSize = true;
+            this.lblNumber.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblNumber.Font = new System.Drawing.Font("Microsoft YaHei UI", 21.85714F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblNumber.Location = new System.Drawing.Point(20, 90);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(62, 70);
+            this.lblNumber.TabIndex = 2;
+            this.lblNumber.Text = "0";
+            this.lblNumber.Click += new System.EventHandler(this.lblNumber_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.85714F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnReset.Location = new System.Drawing.Point(420, 341);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(192, 62);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.Text = "Сброс";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.groupBox1.Controls.Add(this.btnUndo);
+            this.groupBox1.Controls.Add(this.lblCountCommands);
+            this.groupBox1.Controls.Add(this.btnReset);
+            this.groupBox1.Controls.Add(this.lblNumber);
+            this.groupBox1.Controls.Add(this.btnCommand2);
+            this.groupBox1.Controls.Add(this.btnCommand1);
+            this.groupBox1.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 39);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(618, 409);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "нажмите Играть в меню";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // lblCountCommands
+            // 
+            this.lblCountCommands.AutoSize = true;
+            this.lblCountCommands.Location = new System.Drawing.Point(20, 386);
+            this.lblCountCommands.Name = "lblCountCommands";
+            this.lblCountCommands.Size = new System.Drawing.Size(29, 28);
+            this.lblCountCommands.TabIndex = 4;
+            this.lblCountCommands.Text = "0";
+            this.lblCountCommands.Click += new System.EventHandler(this.lblCountCommands_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(28, 28);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(271, 40);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(270, 36);
+            this.toolStripMenuItem1.Text = "toolStripMenuItem1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(28, 28);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menu1ToolStripMenuItem,
+            this.abountToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(642, 38);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menu1ToolStripMenuItem
+            // 
+            this.menu1ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.commandToolStripMenuItem});
+            this.menu1ToolStripMenuItem.Name = "menu1ToolStripMenuItem";
+            this.menu1ToolStripMenuItem.Size = new System.Drawing.Size(80, 34);
+            this.menu1ToolStripMenuItem.Text = "Файл";
+            this.menu1ToolStripMenuItem.Click += new System.EventHandler(this.menu1ToolStripMenuItem_Click);
+            // 
+            // commandToolStripMenuItem
+            // 
+            this.commandToolStripMenuItem.Name = "commandToolStripMenuItem";
+            this.commandToolStripMenuItem.Size = new System.Drawing.Size(315, 40);
+            this.commandToolStripMenuItem.Text = "Играть";
+            this.commandToolStripMenuItem.Click += new System.EventHandler(this.commandToolStripMenuItem_Click);
+            // 
+            // abountToolStripMenuItem
+            // 
+            this.abountToolStripMenuItem.Name = "abountToolStripMenuItem";
+            this.abountToolStripMenuItem.Size = new System.Drawing.Size(161, 34);
+            this.abountToolStripMenuItem.Text = "О программе";
+            this.abountToolStripMenuItem.Click += new System.EventHandler(this.оПрограммеToolStripMenuItem_Click);
+            // 
+            // btnUndo
+            // 
+            this.btnUndo.Font = new System.Drawing.Font("Lucida Console", 14.14286F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnUndo.Location = new System.Drawing.Point(420, 198);
+            this.btnUndo.Name = "btnUndo";
+            this.btnUndo.Size = new System.Drawing.Size(192, 62);
+            this.btnUndo.TabIndex = 5;
+            this.btnUndo.Text = "Отменить";
+            this.btnUndo.UseVisualStyleBackColor = true;
+            this.btnUndo.Click += new System.EventHandler(this.btnUndo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(642, 460);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.groupBox1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCommand1;
+        private System.Windows.Forms.Button btnCommand2;
+        private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblCountCommands;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menu1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem commandToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abountToolStripMenuItem;
+        private System.Windows.Forms.Button btnUndo;
+    }
+}
+
---
+++ lesson07/task0701/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+ * Павел Корженко
+ */
+
+/*
+ * 1. а) Добавить в программу «Удвоитель» подсчёт количества отданных команд удвоителю.
+ *    б) Добавить меню и команду «Играть». При нажатии появляется сообщение, какое число 
+ *       должен получить игрок. Игрок должен получить это число за минимальное количество ходов.
+ *    в) *Добавить кнопку «Отменить», которая отменяет последние ходы. Используйте обобщенный 
+ *       класс Stack
+ */
+
+namespace task0701
+{
+    public partial class Form1 : Form
+    {
+        private int globalNum = 0;
+        Stack st = new Stack();
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Удвоитель";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lblCountCommands.Text = (int.Parse(lblCountCommands.Text) + 1).ToString();
+            lblNumber.Text = (int.Parse(lblNumber.Text) + 1).ToString();
+            // играем!
+            CheckGlobalNum();
+            st.Push('+');
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            lblCountCommands.Text = (int.Parse(lblCountCommands.Text) + 1).ToString();
+            lblNumber.Text = (int.Parse(lblNumber.Text) * 2).ToString();
+            // играем!
+            CheckGlobalNum();
+            st.Push('*');
+        }
+
+        private void CheckGlobalNum()
+        {
+            if (globalNum != 0)
+            {
+                int currentNum = int.Parse(lblNumber.Text);
+                if (globalNum == currentNum)
+                {
+                    string msg = string.Format("Поздоравялем!\nВы получили число {0} за {1} ходов.\nСпасибо.", globalNum, lblCountCommands.Text);
+                    MessageBox.Show(msg);
+                    groupBox1.Text = string.Format("Нажмите в меню - Играть");
+                    ResetValues();
+                } else if(globalNum < currentNum)
+                {
+                    string msg = string.Format("Вы привысили число {0} -> {1}.\nКоличество ходов {2}.\nСпасибо.", globalNum, currentNum, lblCountCommands.Text);
+                    MessageBox.Show(msg);
+                    groupBox1.Text = string.Format("Нажмите в меню - Играть");
+                    ResetValues();
+                }
+            }
+        }
+
+        private void btnUndo_Click(object sender, EventArgs e)
+        {
+            if (st.Count > 0)
+            {
+                switch (st.Pop())
+                {
+                    case '+':
+                        lblNumber.Text = (int.Parse(lblNumber.Text) - 1).ToString();
+                        break;
+                    case '*':
+                        lblNumber.Text = (int.Parse(lblNumber.Text) / 2).ToString();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private void ResetValues()
+        {
+            lblCountCommands.Text = "0";
+            lblNumber.Text = "1";
+            st = new Stack();
+        }
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            ResetValues();
+        }
+
+        private void lblNumber_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblCountCommands_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void commandToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+            Random r = new Random();
+            globalNum = r.Next(1, 100);
+            string msg = string.Format("Получите число {0} за наименьшее количество ходов.\nУдачи", globalNum);
+            MessageBox.Show(msg);
+            groupBox1.Text = string.Format("Играем! Получаем число {0}", globalNum);
+        }
+
+        private void menu1ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void оПрограммеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Угадайка. версия 0.1.\n(c) GeekBrains");
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
